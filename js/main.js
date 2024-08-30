692cbe9fa64c9fed28e05bca9cd4741be56a495f
Dimitri. J <jmukhadze.dimitri@gmail.com>
2024-08-31 01:09:20 +0400

    Slider functionality and design updates

--- js/main.js
+++ js/main.js
@@ -45,3 +45,19 @@ portfolioCloseBtn.forEach(button => {
     })
   })
 })
+
+// SLIDER
+
+const swiper = new Swiper(".client-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
