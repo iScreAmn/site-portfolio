0991fbf802bc4c6f5663615da0be1d7598a88fa1
Dimitri. J <jmukhadze.dimitri@gmail.com>
2024-12-11 14:21:57 +0400

    Fixed a bug with modal windows in Services & Portfolio sections

--- js/main.js
+++ js/main.js
@@ -99,33 +99,7 @@ function enableScrollReveal() {
   console.log("function work")
 }
 
-// Theme Light & Dark
 
-const themeBtn = document.querySelector(".theme-btn")
-// Функция для получения текущей темы
-const getCurrentTheme = () => document.body.classList.contains("dark-theme") ? "dark" : "light"
-
-// Функция для получения текущего значка
-const getCurrentIcon = () => themeBtn.classList.contains("sun") ? "sun" : "moon"
-
-// Слушатель событий для переключения темы
-themeBtn.addEventListener("click", function() {
-  document.body.classList.toggle("dark-theme")
-  themeBtn.classList.toggle("sun")
-
-  localStorage.setItem("saved-theme", getCurrentTheme())
-  localStorage.setItem("saved-icon", getCurrentIcon())
-})
-
-const savedTheme = localStorage.getItem("saved-theme")
-const savedIcon = localStorage.getItem("saved-icon")
-
-// Применяем сохраненную тему и значок
-
-if(savedTheme) {
-  document.body.classList[savedTheme === "dark" ? "add" : "remove"]("dark-theme")
-  themeBtnclassList[savedIcon === "sun" ? "add" : "remove"]("sun")
-}
 
 // Services section - Modal
 
@@ -217,3 +191,30 @@ navLinks.forEach(function(link) {
   })
 })
 
+// Theme Light & Dark
+
+const themeBtn = document.querySelector(".theme-btn")
+// Функция для получения текущей темы
+const getCurrentTheme = () => document.body.classList.contains("dark-theme") ? "dark" : "light"
+
+// Функция для получения текущего значка
+const getCurrentIcon = () => themeBtn.classList.contains("sun") ? "sun" : "moon"
+
+// Слушатель событий для переключения темы
+themeBtn.addEventListener("click", function() {
+  document.body.classList.toggle("dark-theme")
+  themeBtn.classList.toggle("sun")
+
+  localStorage.setItem("saved-theme", getCurrentTheme())
+  localStorage.setItem("saved-icon", getCurrentIcon())
+})
+
+const savedTheme = localStorage.getItem("saved-theme")
+const savedIcon = localStorage.getItem("saved-icon")
+
+// Применяем сохраненную тему и значок
+
+if(savedTheme) {
+  document.body.classList[savedTheme === "dark" ? "add" : "remove"]("dark-theme")
+  themeBtnclassList[savedIcon === "sun" ? "add" : "remove"]("sun")
+}
